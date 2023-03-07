8d294c1ed0677f36ebbc28f661bb850d61e05de5
FILIP <fisk0117@gmail.com>
2023-03-07 20:12:47 +0100

    Enemies!!

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    Rigidbody2D body;
+
     public float range;
     public float speed;
+
+    Vector2 direction;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,10 +22,15 @@ public class EnemyMovement : MonoBehaviour
         Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, range);
         for (int i = 0; i < col.Length; i++)
         {
-            if (col[i].tag == "player")
+            if (col[i].tag == "Player")
             {
+                direction = (col[i].transform.position - transform.position).normalized;
 
             }
         }
+    }
+    void FixedUpdate()
+    {
+        body.velocity = direction * speed;
     }
 }
