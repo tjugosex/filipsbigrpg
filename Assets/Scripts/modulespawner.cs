d9f6096d81cbe542a2db962dadb13bf8e556e305
tjugosex <mikael.skaremyr.te1@hotmail.com>
2023-03-08 08:58:28 +0100

    WUT de hel epic game generation

---
+++ Assets/Scripts/modulespawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class modulespawner : MonoBehaviour
+{
+    public float modulesnmbr = 5;
+    public GameObject moduleprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawncord = (modulesnmbr -1) * -50;
+        float spawnx = spawncord;
+        float spawny = spawncord;
+
+        for (int i = 0; i < modulesnmbr; i++)
+        {
+            for (int k = 0; k < modulesnmbr; k++)
+            {
+                
+                Instantiate(moduleprefab, new Vector3(spawnx, spawny, 20), Quaternion.identity);
+                spawnx += 100;
+            }
+            spawnx = spawncord;
+            spawny += 100;
+        }
+    }
+
+    // Update is called once per frame
+
+}
---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject treeprefab; // the prefab to be instantiated
+
+    void Start()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Vector2 randomPosition = new Vector2(Random.Range(-50f, 50f), Random.Range(-50f, 50f)); 
+            Instantiate(treeprefab, new Vector3 (transform.position.x, transform.position.y, 0) + new Vector3(randomPosition.x, randomPosition.y, 0f), Quaternion.identity); // instantiates the prefab at the random position
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
