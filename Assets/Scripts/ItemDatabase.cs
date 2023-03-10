218669fcf85d2ca7efeaeae20ba85c2b798b6d73
tjugosex <mikael.skaremyr.te1@hotmail.com>
2023-03-10 15:44:01 +0100

    hej

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    public void Awake(){
+        BuildDatabase();
+    }
+
+    public Item GetItem(string itemName)
+    {
+        return items.Find(item => item.title == itemName);
+    }
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>(){
+            new Item(0,"Coins", "monei", new Dictionary<string, int>{
+                {"Value", 1}
+            }),
+            new Item(1,"Fireball Staff", "Ball of fireball of staff :)", new Dictionary<string, int>{
+                {"Type", 0},
+                {"Power", 1}
+            }),
+            new Item(2,"Waterball Staff", "Ball of waterball of staff :)", new Dictionary<string, int>{
+                {"Type", 1},
+                {"Power", 1}
+            })
+            
+            
+            
+            
+        };
+
+    }
+}
---
+++ Assets/Scripts/UIInventory.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    public List<UIItem> uIItems = new List<UIItem>();
+    public GameObject slotPrefab;
+    public Transform slotPanel;
+    public int numberOfSlots = 16;
+
+    private void Awake(){
+        for(int i = 0; i < numberOfSlots; i++){
+            GameObject instance = Instantiate(slotPrefab);
+            instance.transform.SetParent(slotPanel);
+            uIItems.Add(instance.GetComponentInChildren<UIItem>());
+        }
+    }
+
+    public void UpdateSlot(int slot, Item item)
+    {
+        uIItems[slot].UpdateItem(item);
+    }
+
+    public void AddNewItem(Item item)
+    {
+        UpdateSlot(uIItems.FindIndex(i => i.item == null), item);
+    }
+
+    public void RemoveNewItem(Item item)
+    {
+        UpdateSlot(uIItems.FindIndex(i => i.item == null), null);
+    }
+}
