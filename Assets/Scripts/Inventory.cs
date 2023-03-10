6999ff9e084afd6765413cf42f5468040db43d26
tjugosex <mikael.skaremyr.te1@hotmail.com>
2023-03-10 15:51:39 +0100

    Update Inventory.cs

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public List<Item> characterItems = new List<Item>();
+    public ItemDatabase ItemDatabase;
+    public UIInventory inventoryUI;
 
+    private void Start()
+    {
+        GiveItem(1);
+    }
+
+    public void GiveItem(int id){
+        Item itemToAdd = ItemDatabase.GetItem(id);
+        characterItems.Add(itemToAdd);
+        inventoryUI.AddNewItem(itemToAdd);
+        Debug.Log("Added item: " + itemToAdd.title);
+    }
+
+    public Item CheckForItem(int id){
+        return characterItems.Find(item => item.id == id);
+    }
+
+    public void RemoveItem(int id)
+    {
+        Item itemToRemove = CheckForItem(id);
+        if (itemToRemove != null)
+        {
+            characterItems.Remove(itemToRemove);
+            inventoryUI.RemoveNewItem(itemToRemove);
+            Debug.Log("Item removed: " + itemToRemove.title);
+        }
+    }
 }
