4406d068b9df62e7a1f24f48ed3d41616b7236c4
FILIP <fisk0117@gmail.com>
2023-03-13 20:49:13 +0100

    Generation

--- Assets/Scripts/Generation/modulespawner.cs
+++ Assets/Scripts/Generation/modulespawner.cs
@@ -17,13 +17,25 @@ public class modulespawner : MonoBehaviour
 
     public Transform player;
     public Vector2 currentPos;
+    public GameObject modul;
+
 
     public Vector2 Test;
 
     List<GameObject> tempModules;
     private void Start()
     {
-        tempModules = modules;
+
+
+        List<int> temp = new List<int>();
+        temp.Add(1);
+        temp.Add(2);
+        temp.Add(0);
+        ChangeTileInfoToPoint(temp, new Vector2(-50,100));
+
+        //Vector2 from = new Vector2(5,5);
+        //Vector2 to = new Vector2(50,50);
+        //ChangeTileInfoInArea(temp, from, to);
     }
     private void Update()
     {
@@ -40,6 +52,7 @@ public class modulespawner : MonoBehaviour
             }
             
         }
+
         Generation();
     }
     void Generation()
@@ -47,27 +60,24 @@ public class modulespawner : MonoBehaviour
 
         currentPos = new Vector2Int(Mathf.FloorToInt(player.position.x / 10f + 1), Mathf.FloorToInt(player.position.y / 10f + 1));
 
-        for (int x = (int)currentPos.x - RenderDistance; x <= (int)currentPos.x + RenderDistance; x++)
+        for (int y = (int)currentPos.y - RenderDistance; y <= (int)currentPos.y + RenderDistance; y++)
         {
-            for (int y = (int)currentPos.y - RenderDistance; y <= (int)currentPos.y + RenderDistance; y++)
+            for (int x = (int)currentPos.x - RenderDistance; x <= (int)currentPos.x + RenderDistance; x++)
             {
+                TilesSet.TryGetValue(new Vector2(x, y), out bool bol);
 
-                
-                if (!TilesSet.TryGetValue(new Vector2(x, y), out bool bol))
+
+                if (!bol)
                 {
-                    tempModules = modules;
+                    tempModules = new List<GameObject>(modules);
 
                     if (Tiles.TryGetValue(new Vector2(x, y), out TileInfo _info))
                     {
-
+                        //Filtrerar ut de moduler some inte ska vara.
                         for (int i = 0; i < tempModules.Count; i++)
                         {
-
                             for (int k = 0; k < _info.objs.Count; k++)
                             {
-
-                                Debug.Log(_info.objs[k]);
-
                                 if (tempModules[i].GetComponent<ModuleInfo>().id == _info.objs[k])
                                 {
                                     tempModules.RemoveAt(i);
@@ -77,11 +87,18 @@ public class modulespawner : MonoBehaviour
                             }
                         }
                     }
+                    if (tempModules.Count == 0)
+                    {
+                        Debug.Log("shit");
+                        tempModules = new List<GameObject>{modul};
+                    }
+                    //Gör modulen och sätter position
                     GameObject mod = Instantiate(tempModules[Random.Range(0, tempModules.Count)]);
+
                     mod.transform.position = new Vector3(x * 10, y * 10, 0f);
                     mod.transform.parent = this.transform;
                     TilesSet.Add(new Vector2(x, y), true);
-
+                    //Ändrar nästkommande information om tiles
                     ChangeTileInfo(mod.GetComponent<ModuleInfo>(), new Vector2(x, y));
                 }
             }
@@ -96,22 +113,12 @@ public class modulespawner : MonoBehaviour
             for (int k = 0; k < pos.Count; k++)
             {
                 TilesSet.TryGetValue(pos[k] + position, out bool bol);
+
                 if (!bol)
                 {
-                    Debug.Log(pos[k] + position);
-
                     if (Tiles.TryGetValue(pos[k] + position, out TileInfo _info))
                     {
                         _info.objs.Add(objcts[i]);
-                        //if (_info.objs[j] != objcts[i])
-                        //{
-                            
-                        //}
-                        //for (int j = 0; j < _info.objs.Count; j++)
-                        //{
-                            
-
-                        //}
                         Tiles.Remove(pos[k] + position);
                         Tiles.Add(pos[k] + position, _info);
                     }
@@ -126,6 +133,71 @@ public class modulespawner : MonoBehaviour
             }
         }
 
+    }
+
+    void ChangeTileInfoInArea(List<int> UnWantedIds, Vector2 from, Vector2 to)
+    {
+        for (int y = (int)from.y; y < to.y; y++)
+        {
+            for (int x = (int)from.x; x < to.x; x++)
+            {
+                for (int i = 0; i < UnWantedIds.Count; i++)
+                {
+                    Vector2 pos = new Vector2(x, y);
+                    TilesSet.TryGetValue(pos, out bool bol);
+
+                    if (!bol)
+                    {
+                        if (Tiles.TryGetValue(pos, out TileInfo _info))
+                        {
+                            _info.objs.Add(UnWantedIds[i]);
+                            Tiles.Remove(pos);
+                            Tiles.Add(pos, _info);
+                        }
+                        else
+                        {
+
+                            TileInfo _tileInfo = new TileInfo();
+                            _tileInfo.objs.Add(UnWantedIds[i]);
+                            Tiles.Add(pos, _tileInfo);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void ChangeTileInfoToPoint(List<int> UnWantedIds, Vector2 to)
+    {
+        float k = to.y / to.x;
+        float y;
+        for (int x = 0; x < Mathf.Abs(to.x); x++)
+        {
+
+            y = x * k * Mathf.Sign(to.x);
+            y = (int)y;
+            Vector2 pos = new Vector2((int)(x * Mathf.Sign(to.x)), y);
+            TilesSet.TryGetValue(pos, out bool bol);
+
+            if (!bol)
+            {
+                for (int i = 0; i < UnWantedIds.Count; i++)
+                {
+                    if (Tiles.TryGetValue(pos, out TileInfo _info))
+                    {
+                        _info.objs.Add(UnWantedIds[i]);
+                        Tiles.Remove(pos);
+                        Tiles.Add(pos, _info);
+                    }
+                    else
+                    {
+
+                        TileInfo _tileInfo = new TileInfo();
+                        _tileInfo.objs.Add(UnWantedIds[i]);
+                        Tiles.Add(pos, _tileInfo);
+                    }
+                }
+            }
+        }
     }
     public class TileInfo
     {
