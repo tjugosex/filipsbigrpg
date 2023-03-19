2dc43a67ea8715027a10e7887907018abf760323
tjugosex <mikael.skaremyr.te1@hotmail.com>
2023-03-19 16:55:53 +0100

    uh

--- Assets/Scripts/GridSnap.cs
+++ Assets/Scripts/GridSnap.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class GridSnap : MonoBehaviour
 {
     public float gridSize = 100f; // Size of the grid cells
     public bool legal = false;
 
- 
+
 
     public float nr = 1;
     float neighboorsint = 0;
@@ -41,18 +41,19 @@ public class GridSnap : MonoBehaviour
             neighborCount++;
         }
 
-        Vector2 size = new Vector2(gridSize * 0.5f, gridSize * 0.5f); // Slightly smaller than gridSize to avoid overlaps
+        Vector2 size = new Vector2(gridSize * 0.5f, gridSize * 0.5f);
         Collider2D[] neighbors2 = new Collider2D[4];
-        neighbors2[0] = Physics2D.OverlapBox(transform.position + new Vector3(0f, gridSize, 0f), size, 0f); // Above
-        neighbors2[1] = Physics2D.OverlapBox(transform.position + new Vector3(gridSize, 0f, 0f), size, 0f); // Right
-        neighbors2[2] = Physics2D.OverlapBox(transform.position + new Vector3(-gridSize, 0f, 0f), size, 0f); // Left
-        neighbors2[3] = Physics2D.OverlapBox(transform.position + new Vector3(0f, -gridSize, 0f), size, 0f); // Below
+        neighbors2[0] = Physics2D.OverlapBox(transform.position + new Vector3(0f, gridSize, 0f), size, 0f); // north
+        neighbors2[1] = Physics2D.OverlapBox(transform.position + new Vector3(gridSize, 0f, 0f), size, 0f); // east
+        neighbors2[2] = Physics2D.OverlapBox(transform.position + new Vector3(-gridSize, 0f, 0f), size, 0f); // west
+        neighbors2[3] = Physics2D.OverlapBox(transform.position + new Vector3(0f, -gridSize, 0f), size, 0f); // south
 
 
 
         // Loop through the neighbors and do something with them
         foreach (Collider2D neighbor in neighbors2)
         {
+
             // Ignore non-grid objects and empty cells
             if (neighbor == null || neighbor.gameObject == gameObject || !neighbor.CompareTag("GridObject"))
             {
@@ -71,9 +72,9 @@ public class GridSnap : MonoBehaviour
                 else
                 {
                     legal = true;
-                    
 
-                   
+
+
                     GameObject[] gridObjects = GameObject.FindGameObjectsWithTag("GridObject");
 
                     foreach (GameObject rr in gridObjects)
@@ -90,6 +91,46 @@ public class GridSnap : MonoBehaviour
             Debug.Log("illegal move");
             Destroy(gameObject);
         }
+
+    }
+
+    void Update()
+    {
+        Vector2 size = new Vector2(gridSize, gridSize) * 0.5f;
+        Collider2D[] neighbors = new Collider2D[4];
+        neighbors[0] = Physics2D.OverlapBox(transform.position + Vector3.up * gridSize, size, 0f); // North
+        neighbors[1] = Physics2D.OverlapBox(transform.position + Vector3.right * gridSize, size, 0f); // East
+        neighbors[2] = Physics2D.OverlapBox(transform.position + Vector3.left * gridSize, size, 0f); // West
+        neighbors[3] = Physics2D.OverlapBox(transform.position + Vector3.down * gridSize, size, 0f); // South
+
+        if (neighbors.Any(n => n != null && n.CompareTag("GridObject")))
+        {
+            if (neighbors[0] != null && neighbors[1] != null && neighbors[0].CompareTag("GridObject") && neighbors[1].CompareTag("GridObject"))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Rails/railerodeNE");
+            }
+            else if (neighbors[1] != null && neighbors[2] != null && neighbors[1].CompareTag("GridObject") && neighbors[2].CompareTag("GridObject"))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Rails/railerodeWE");
+            }
+            else if (neighbors[2] != null && neighbors[3] != null && neighbors[2].CompareTag("GridObject") && neighbors[3].CompareTag("GridObject"))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Rails/railerodeWS");
+            }
+            else if (neighbors[0] != null && neighbors[2] != null && neighbors[0].CompareTag("GridObject") && neighbors[2].CompareTag("GridObject"))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Rails/railerodeWN");
+            }
+            else if (neighbors[0] != null && neighbors[3] != null && neighbors[0].CompareTag("GridObject") && neighbors[3].CompareTag("GridObject"))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Rails/railerodeNS");
+            }
+            else if (neighbors[1] != null && neighbors[3] != null && neighbors[1].CompareTag("GridObject") && neighbors[3].CompareTag("GridObject"))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Rails/railerodeES");
+            }
+        }
+
 
     }
 
